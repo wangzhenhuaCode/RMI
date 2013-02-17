d3a5f15459f1b0fe7b79d02f03822829187eb2b0
wangzhenhuaCode <wangzhenhua.mail@gmail.com>
2013-02-17 16:23:11 -0500

    df

--- RMI/src/Naming/RegistryMessageProcessor.java
+++ RMI/src/Naming/RegistryMessageProcessor.java
@@ -8,7 +8,9 @@ public class RegistryMessageProcessor implements MessageProcessor {
 	@Override
 	public Message process(Message message) {
 		// TODO Auto-generated method stub
+		if(message.getMessageType()==Message.BIND_TO_REGISTERY){
 		return null;
+		}else if(message.getMessageType())
 	}
 
 
