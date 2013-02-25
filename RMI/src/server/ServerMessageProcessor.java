6bbf76e159663f0cc641d2a25a2951c1b3e26c08
GameOwen <gameowencz@gmail.com>
2013-02-24 23:17:25 -0500

    Signed-off-by: GameOwen <gameowencz@gmail.com>

--- RMI/src/server/ServerMessageProcessor.java
+++ RMI/src/server/ServerMessageProcessor.java
@@ -9,60 +9,57 @@ import util.Message;
 import util.MessageProcessor;
 import util.Remote;
 
-
 public class ServerMessageProcessor implements MessageProcessor {
 
-	
 	public Message process(Message message) {
-		Message newMessage=new Message();	
-		 Object obj=DataTable.getInstance().get(message.getReference());
-		 Object val=null;
-		 
-		synchronized(obj){
-			Class<?> type=obj.getClass();
-			Class<?> paramaterType[]=new Class<?>[message.getArgs().length];
-			for(int i=0;i<message.getArgs().length;i++){
-				paramaterType[i]=message.getArgs()[i].getClass();
+		Message newMessage = new Message();
+		Object obj = DataTable.getInstance().get(message.getReference());
+		Object val = null;
+
+		synchronized (obj) {
+			Class<?> type = obj.getClass();
+			Class<?> paramaterType[] = new Class<?>[message.getArgs().length];
+			for (int i = 0; i < message.getArgs().length; i++) {
+				paramaterType[i] = message.getArgs()[i].getClass();
 			}
 			Method method = null;
 			try {
 				method = type.getMethod(message.getMethod(), paramaterType);
 			} catch (SecurityException e) {
 				// TODO Auto-generated catch block
-				newMessage.setErrorMessage("Remote error: "+e.toString());
+				newMessage.setErrorMessage("Remote error: " + e.toString());
 				return newMessage;
 			} catch (NoSuchMethodException e) {
 				// TODO Auto-generated catch block
-				newMessage.setErrorMessage("Remote error: "+e.toString());
+				newMessage.setErrorMessage("Remote error: " + e.toString());
 				return newMessage;
 			}
 			try {
-				val= method.invoke(obj, message.getArgs());
+				val = method.invoke(obj, message.getArgs());
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
-				newMessage.setErrorMessage("Remote error: "+e.toString());
+				newMessage.setErrorMessage("Remote error: " + e.toString());
 				return newMessage;
 			}
 			DataTable.getInstance().put(message.getReference(), obj);
 		}
-		Class<?> interfaces[]=val.getClass().getInterfaces();
-		boolean remotabe=false;
-		for(int j=0;j<interfaces.length;j++){
-			if(interfaces[j].toString().equals(Remote.class.toString())){
-				remotabe=true;
+		Class<?> interfaces[] = val.getClass().getInterfaces();
+		boolean remotabe = false;
+		for (int j = 0; j < interfaces.length; j++) {
+			if (interfaces[j].toString().equals(Remote.class.toString())) {
+				remotabe = true;
 			}
 		}
-		
+
 		newMessage.setRemote(remotabe);
-		try{
+		try {
 			newMessage.setValue((Serializable) val);
-		}catch(Exception e){
-			newMessage.setErrorMessage("Remote error: "+e.toString());
+		} catch (Exception e) {
+			newMessage.setErrorMessage("Remote error: " + e.toString());
 			return newMessage;
 		}
 		return newMessage;
-		
-		
+
 	}
-	
+
 }
